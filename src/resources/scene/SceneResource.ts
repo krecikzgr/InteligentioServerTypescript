64e6fa5438cdf00fcc3f0f05b51923353d2af1ee
Adrian <kaleciarz@gmail.com>
2019-02-13 22:50:48 +0100

    WIP

--- src/resources/scene/SceneResource.ts
+++ src/resources/scene/SceneResource.ts
@@ -1,7 +1,8 @@
 import {Request, Response, Next} from 'restify';
-import {Resource} from '../Resource'
-import {sceneService} from './SceneService'
-import {HttpServer} from '../../httpServer'
+import {Resource} from '../Resource';
+import {sceneService} from './SceneService';
+import {HttpServer} from '../../httpServer';
+import {ResponseBuilder} from '../../utilities/ResponseBuilder';
 
 export class SceneResource implements Resource {
     public initialize(server: HttpServer):void {
@@ -10,7 +11,11 @@ export class SceneResource implements Resource {
     }
 
     private async list(req: Request, res: Response): Promise<void> {
-        res.send(await sceneService.list());
+        const responseBuilder = new ResponseBuilder();
+        const data = await sceneService.list();
+        responseBuilder.withData(data)
+        .withMessage("Objects")
+        .build(res);
     }
 
     private async create(req: Request, res: Response): Promise<void> {
