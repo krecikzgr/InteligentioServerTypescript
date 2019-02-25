ac2aae8f397ddd2297e401a047b08174c8c7de8b
Adrian <kaleciarz@gmail.com>
2019-02-26 00:06:59 +0100

    Some questions to ask

--- src/ObjectDecorator.ts
+++ src/ObjectDecorator.ts
@@ -1,11 +1,37 @@
 export interface ObjectDecorator<T> {
     //HOW TO declare async interface
-    decorate(object:T): Promise<T>
-    digest(object:T): Promise<T>
+    decorate(object: T): Promise<T>
+    digest(object: T): Promise<T>
 }
 
-export interface DecoratorService<T> {
-    decorators:ObjectDecorator<T>[];
-    decorate(object:T): Promise<T>
-    digest(object:T): Promise<T>
+export class DecoratorService<T> {
+    decorators: ObjectDecorator<T>[];
+
+    constructor() {
+        this.decorators = []
+    }
+
+    addDecorator(decorator: ObjectDecorator<T>) {
+        this.decorators.push(decorator);
+    }
+
+    decorate(object: T): Promise<T> {
+        var localObject = object;
+        this.decorators.forEach(async (decorator) => {
+            localObject = await decorator.decorate(localObject);
+        })
+        return new Promise((resolve) => {
+            resolve(localObject);
+        })
+    }
+
+    digest(object: T): Promise<T> {
+        var localObject = object;
+        this.decorators.forEach(async (decorator) => {
+            localObject = await decorator.digest(localObject);
+        })
+        return new Promise((resolve) => {
+            resolve(localObject);
+        })
+    }
 }
--- src/resources/room/RoomService.ts
+++ src/resources/room/RoomService.ts
@@ -1,7 +1,27 @@
 import { Room } from "./Room";
-import {DatabaseProvider} from '../../utilities/Database';
-import {sensorService} from '../sensor/SensorService';
-import {roomDecoratorService} from './decorators/RoomDecoratorService';
+import { DatabaseProvider } from '../../utilities/Database';
+import { sensorService } from '../sensor/SensorService';
+import { roomDecoratorService } from './decorators/RoomDecoratorService';
+import { DecoratorService } from "../../ObjectDecorator";
+import { runInThisContext } from "vm";
+
+
+export class ObjectService<T> {
+    decoratorService: DecoratorService<T>;
+
+    constructor() {
+        this.decoratorService = new DecoratorService<T>();
+    }
+
+    public async list(): Promise<T[]> {
+        const connection = await DatabaseProvider.getConnection();
+        //How to pass a class Name as a variable
+        const objects = await connection.getRepository(T).find();
+        return await Promise.all(objects.map(async (object) => {
+            return await this.decoratorService.decorate(object);
+        }));
+    }
+}
 
 export class RoomService {
     public async create(scene: Room): Promise<Room> {
@@ -17,16 +37,16 @@ export class RoomService {
     public async list(): Promise<Room[]> {
         const connection = await DatabaseProvider.getConnection();
         const rooms = await connection.getRepository(Room).find();
-        return await Promise.all(rooms.map( async (room) => {
+        return await Promise.all(rooms.map(async (room) => {
             return await roomDecoratorService.decorate(room);
         }));
     }
 
-    public async activate(id:number, isActive:boolean): Promise<Room> {
+    public async activate(id: number, isActive: boolean): Promise<Room> {
         const connection = await DatabaseProvider.getConnection();
-        const room =  await connection.getRepository(Room).findOne(id);
-        
-        await Promise.all(room.sensors.map( async (sensor) => {
+        const room = await connection.getRepository(Room).findOne(id);
+
+        await Promise.all(room.sensors.map(async (sensor) => {
             sensorService.activate(sensor.id, isActive);
         }));
         return await connection.getRepository(Room).save(room);
