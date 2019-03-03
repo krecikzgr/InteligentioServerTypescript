75647135129b3318dc71e97c3ee078928e3a8a45
Adrian <kaleciarz@gmail.com>
2019-03-03 21:08:26 +0100

    Udated for mocked value from sensor

--- src/resources/scene/SceneResource.ts
+++ src/resources/scene/SceneResource.ts
@@ -5,20 +5,72 @@ import {HttpServer} from '../../httpServer';
 import {ResponseBuilder} from '../../utilities/ResponseBuilder';
 
 export class SceneResource implements Resource {
-    public initialize(server: HttpServer):void {
+    public initialize(server: HttpServer): void {
         server.get("/scene", this.list);
         server.post("/scene/", this.create.bind(this))
+        server.get("/scene/:id", this.get)
+        server.patch("/scene/:id", this.update)
     }
 
-    private async list(req: Request, res: Response): Promise<void> {
+    private async list(req: Request, res: Response) {
         const responseBuilder = new ResponseBuilder();
-        const data = await sceneService.list();
-        responseBuilder.withData(data)
-        .withMessage("Objects")
-        .build(res);
+        try {
+            const data = await sceneService.list();
+            responseBuilder.withData(data)
+                .withMessage("Objects")
+                .build(res);
+        } catch (e) {
+            console.log(e)
+            responseBuilder
+                .withHttpResourceNotAvailable()
+                .withMessage(e.message)
+                .build(res)
+        }
     }
 
-    private async create(req: Request, res: Response): Promise<void> {
-        res.send(await sceneService.create(req.body));
+    private async get(req: Request, res: Response) {
+        const responseBuilder = new ResponseBuilder();
+        try {
+            const data = await sceneService.getById(req.params.id)
+            responseBuilder.withData(data)
+                .withMessage("Object")
+                .build(res);
+        } catch (e) {
+            responseBuilder
+                .withHttpResourceNotAvailable()
+                .withMessage(e.message)
+                .build(res)
+        }
+    }
+
+    private async create(req: Request, res: Response) {
+        const responseBuilder = new ResponseBuilder();
+        try {
+            const data = await sceneService.create(req.body)
+            responseBuilder.withData(data)
+                .withMessage("Object")
+                .build(res);
+        } catch (e) {
+            responseBuilder
+                .withHttpResourceNotAvailable()
+                .withMessage(e.message)
+                .build(res)
+        }
+    }
+
+    private async update(req: Request, res: Response): Promise<void> {
+        const responseBuilder = new ResponseBuilder();
+        try {
+            const data = await sceneService.update(req.params.id, req.body)
+            responseBuilder
+                .withMessage("Object")
+                .withData(data)
+                .build(res)
+        } catch (e) {
+            responseBuilder
+                .withHttpResourceNotAvailable()
+                .withMessage(e.message)
+                .build(res)
+        }
     }
 }
