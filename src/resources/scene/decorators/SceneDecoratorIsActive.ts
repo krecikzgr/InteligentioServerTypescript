7371c7d78e852141809b45e749cca5fac42ecc22
Adrian <kaleciarz@gmail.com>
2019-03-05 23:13:18 +0100

    Fixed sceneDecoratorIsActive decorate method

--- src/resources/scene/decorators/SceneDecoratorIsActive.ts
+++ src/resources/scene/decorators/SceneDecoratorIsActive.ts
@@ -2,25 +2,20 @@ import { ObjectDecorator } from '../../../ObjectDecorator';
 import { Scene } from '../Scene';
 import { sceneSettingService } from '../../sceneSetting/SceneSettingService';
 import { sensorService } from '../../sensor/SensorService';
-import { sceneService } from '../SceneService';
 
 
 export class SceneDecoratorIsActive implements ObjectDecorator<Scene> {
     async decorate(object: Scene): Promise<Scene> {
         const localObject = object;
         const sceneSettings = await sceneSettingService.listForScene(object.id)
-        let value: boolean = false;
-        console.log("START DECORATING  number of scene settings " + sceneSettings.length)
+        let value: boolean = true;
         await Promise.all(sceneSettings.map(async (sceneSetting) => {
             try {
                 const sensor = await sensorService.getById(sceneSetting.sensorId);
-                console.log("control sensor with name " + sensor.name);
                 value = ( sensor.isActive == sceneSetting.isActive ) && value
             } catch (e) {
-                console.log("No sensor for sensor setting")
                 value = false
             }
-            console.log("STOP DECORATING")
         }));
         localObject.isActive = value;
         return localObject;
