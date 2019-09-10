310e154b0c205e7880f76f5954ca44289a38f165
akaleta <akaleta@gsd-software.com>
2019-09-10 23:12:38 +0200

    Update access

--- src/Index.ts
+++ src/Index.ts
@@ -2,11 +2,21 @@ import "reflect-metadata";
 import { ApiServer } from './App'
 import { DatabaseProvider } from './utilities/Database';
 
+// DatabaseProvider.configure({
+//     type: process.env.DATABASE_TYPE as any || 'postgres',
+//     database: process.env.DATABASE_NAME || 'inteligentio',
+//     username: process.env.DATABASE_USERNAME || 'adrian',
+//     password: process.env.DATABASE_PASSWORD || 'adrian',
+//     host: process.env.DATABASE_HOST || 'localhost',
+//     port: +process.env.DATABASE_PORT || 5432,
+//     ssl: !!process.env.USE_SSL
+// });
+
 DatabaseProvider.configure({
     type: process.env.DATABASE_TYPE as any || 'postgres',
-    database: process.env.DATABASE_NAME || 'inteligentio',
-    username: process.env.DATABASE_USERNAME || 'adrian',
-    password: process.env.DATABASE_PASSWORD || 'adrian',
+    database: process.env.DATABASE_NAME || 'inteligentio2',
+    username: process.env.DATABASE_USERNAME || 'admin',
+    password: process.env.DATABASE_PASSWORD || 'admin',
     host: process.env.DATABASE_HOST || 'localhost',
     port: +process.env.DATABASE_PORT || 5432,
     ssl: !!process.env.USE_SSL
